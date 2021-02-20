0028aa9eab4526e7a83800dc605e8c809e007213
Antoine <antoinedaigle2108rhotmail.com>
2021-02-20 00:08:58 -0500

    Badaboom poutpout

--- Poutre.m
+++ Poutre.m
@@ -12,18 +12,19 @@ E = 2.7e9; %Notre module: 2.7e9
 dens = 1937.18;
 mu = dens*b*h;
 
-B = 5; %Amortissement
+B = 3.6; %Amortissement
+
 
 dt = 6e-5;
 
 nx = 20; %Nombre d'éléments spaciaux
-nt = 5000;
+nt = 9000;
 
 dx = L/(nx-1); %Pour la résolution du graph?
 x = -dx:dx:L+dx; %Matrice d'éléments spaciaux
 nx = nx + 2; %Nombre d'élément (+2 pour ajuster avec le dx de droit pis de gauche)
 
-dx_n = dx; % Normalise that shit
+dx_n = dx; % Normalise that shit %ATTENTION NOUS AVONS PAS NORMALISÉ
 
 kappa = sqrt(E*J/(mu*L^4)); %Chunk
 
@@ -31,14 +32,16 @@ mu_simu = kappa*dt/dx_n^2;
 
 
 
-if(mu_simu > 0.5)
-    warning("La simulation ne sera pas stable")
-end
+% if(mu_simu > 0.5)
+%     warning("La simulation ne sera pas stable")
+% end
 
-f1 = 1.875^2*kappa/(2*pi); %Dans un ti-power-point slides 34
+% f1 = 1.875^2*kappa/(2*pi); %Dans un ti-power-point slides 34
 
 
 %Conditions initales
+F = zeros(1, nx); %Force
+F(10) = -1;
 temps = 0;
 w = 1 * fliplr(0.001*sin(pi*x/(2*L))-0.001);
 w_old = w; %Un pas dans le passé
@@ -68,8 +71,8 @@ for n=0:nt
     
     w_new = zeros(1, nx);
     i = 3:nx-2;
-    %w_new(i) = coeff1*w(i) + coeff2*(w(i+1)+w(i-1))+coeff3*(w(i+2)+w(i-2))-w_old(i);  % La grande ligne
-    w_new(i) = (-coeff1*(w(i+2)-4*w(i+1)+6*w(i)-4*w(i-1)+w(i-2)) - coeff2*(-2*w(i)+w_old(i)) + coeff3*w(i))*coeff4;
+    %w_new(i) = coeff1*w(i) + coeff2*(w(i+1)+w(i-1))+coeff3*(w(i+2)+w(i-2))-w_old(i);  %La grande ligne
+    w_new(i) = (-coeff1*(w(i+2)-4*w(i+1)+6*w(i)-4*w(i-1)+w(i-2)) - coeff2*(-2*w(i)+w_old(i)) + coeff3*w(i) + F(i)/dx_n)*coeff4; %La grosse ligne
     
     %condi limit
     w_new(1:2) = 0;
@@ -88,38 +91,3 @@ end
 figure 
 plot((0:dt:(nt*dt)), masse_bout)
 grid
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%Fin de ma tentative pété
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
