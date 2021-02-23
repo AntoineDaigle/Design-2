0268b66355c1b4df22b5ed3b8db44b995f71e88f
Antoine <antoinedaigle2108rhotmail.com>
2021-02-22 20:41:15 -0500

    Ajustement

--- Mesure de frequence.py
+++ Mesure de frequence.py
@@ -7,15 +7,18 @@ from scipy.signal import sosfilt
 import pandas as pd
 
 
-position = r"C:\Users\antoi\Desktop\testest.csv"
+
+# position = r"Test capteur de position\Prise de mesure 1\F0001CH1.CSV"
+position = r"Test capteur de position\Prise de mesure 2\F0002CH1.CSV"
 df = pd.read_csv(position, index_col=False)
 
-#Pour voir la forme du CSV:
-#print(df.head())
+# Pour voir la forme du CSV:
+# print(df.head())
 
+
+# Tentative de filtre
 # sos = butter(5, 4) #aquisition en milliseconde
 # filtered = sosfilt(sos, df["Tension"])
-
 
 Peak = find_peaks(df["Tension"], distance=50)
 
@@ -27,15 +30,17 @@ for i in Peak[0]:
     Temps_peak.append(df["Temps"][i])
     Tension_peak.append(df["Tension"][i])
 
-#Maxi du peak
-Maxi = df.loc[df['Tension'].idxmax()]
-print(Maxi)
+# Maxi du peak
+index = Tension_peak.index(max(Tension_peak))
+
+Temps_peak = Temps_peak[index:]
+Tension_peak = Tension_peak[index:]
 
 
 def func(Temps_peak, a, b, d):
     return a * np.exp(-b/Temps_peak) + d
 
-param, param_cova = curve_fit(func, Temps_peak, Tension_peak)
+param, param_cova = curve_fit(func, Temps_peak, Tension_peak, maxfev=5000)
 
 print("Les paramètres sont calculés, l'ammortissement est de: {}".format(param[1]))
 
@@ -43,5 +48,7 @@ plt.plot(df["Temps"], df["Tension"], label="Données brutes")
 plt.plot(Temps_peak, Tension_peak, label="Sommet")
 plt.plot(Temps_peak, func(Temps_peak, *param), label="Curve_fit")
 plt.legend()
+plt.xlabel("Temps")
+plt.ylabel("Signal")
 plt.show()
 
