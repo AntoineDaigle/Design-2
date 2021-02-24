c3956df4dc338e463a4bccadf92969fb7f4e0909
Antoine <antoinedaigle2108rhotmail.com>
2021-02-23 21:42:41 -0500

    Call me Mr. Net cause this is clean now

--- Mesure de frequence.py
+++ Mesure de frequence.py
@@ -12,18 +12,31 @@ Returns:
     matplotlib : Graphique  de l'oscillation de la lame en fonction du temps
 """
 
+#################### Chercher le fichier
 
 position = r"Test capteur de position\Prise de mesure 1\F0001CH1.CSV"
 # position = r"Test capteur de position\Prise de mesure 2\F0002CH1.CSV"
 df = pd.read_csv(position, index_col=False)
+plt.plot(df["Temps"], df["Tension"], label="Données brutes")
 
-# Pour voir la forme du CSV:
-# print(df.head())
+# print(df.head())  # Pour voir la forme du CSV:
 
 
-# Tentative de filtre
+
+
+
+
+
+##################### Tentative de filtre
 # sos = butter(5, 4) #aquisition en milliseconde
 # filtered = sosfilt(sos, df["Tension"])
+
+
+
+
+
+
+################## Sommets des données
 
 Peak = find_peaks(df["Tension"], distance=50)
 
@@ -34,12 +47,33 @@ for i in Peak[0]:
     Temps_peak.append(df["Temps"][i])
     Tension_peak.append(df["Tension"][i])
 
-# Maxi du peak
+
+
+
+
+
+
+
+################## Curve_fit
+
 index = Tension_peak.index(max(Tension_peak))
 
 Temps_peak = Temps_peak[index:]
 Tension_peak = Tension_peak[index:]
 
+def func(Temps_peak, a, b, d):
+    return a * np.exp(-b/Temps_peak) + d
+
+param, param_cova = curve_fit(func, Temps_peak, Tension_peak, maxfev=5000)
+print("Les paramètres sont calculés, l'ammortissement est de: {}".format(param[1]))
+
+plt.plot(Temps_peak, func(Temps_peak, *param), label="Curve_fit")
+plt.plot(Temps_peak, Tension_peak, label="Sommet")
+
+
+
+
+##################### Section calcul de la fréquence d'oscillation
 
 def frequence_osci(Temps, num_peak):
     """Déterminer la fréquence d'oscillation de la lame.
@@ -61,26 +95,23 @@ def frequence_osci(Temps, num_peak):
         else:
             w_0.append(1/(temps[i+1] - temps[i]))
 
-freq_temps = [Temps_peak[0], Temps_peak[1]]
-freq_tensi = [Tension_peak[0], Tension_peak[1]]
+Nombre_sommets = 8  # Sélectionne le nombre de sommet pour le calcul
+
+freq_temps = Temps_peak[:Nombre_sommets]
+freq_tensi = Tension_peak[:Nombre_sommets]
+print(frequence_osci(Temps_peak, Nombre_sommets))
+plt.scatter(freq_temps, freq_tensi, color="red", label=r"Sommets utilisés pour $\omega_0$")
 
 
-def func(Temps_peak, a, b, d):
-    return a * np.exp(-b/Temps_peak) + d
 
-param, param_cova = curve_fit(func, Temps_peak, Tension_peak, maxfev=5000)
 
-print("Les paramètres sont calculés, l'ammortissement est de: {}".format(param[1]))
 
-plt.plot(df["Temps"], df["Tension"], label="Données brutes")
-plt.plot(Temps_peak, Tension_peak, label="Sommet")
-plt.scatter(freq_temps, freq_tensi, color="red", label=r"Sommet utilisé pour $\omega_0$")
-plt.plot(Temps_peak, func(Temps_peak, *param), label="Curve_fit")
+###################### Génération du graphique
+
 plt.legend()
 plt.xlabel("Temps [s]")
 plt.ylabel("Signal [V]")
 
-
-print(frequence_osci(Temps_peak, 8))
+# plt.savefig("Oscillation de la lame", dpi=600)    # Pour sauvegarder la figure, don't uncomment this fucking line if you don't want to save 3489 figures
 plt.show()
 
