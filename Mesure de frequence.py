8a97f0dfefe3ad972a1c6ad07672920eda4e0cb8
Antoine <antoinedaigle2108rhotmail.com>
2021-03-03 21:44:05 -0500

    clean up in ailse 4

--- Mesure de frequence.py
+++ Mesure de frequence.py
@@ -7,10 +7,10 @@ from scipy.signal import sosfilt
 import pandas as pd
 import os
 
-"""Script permetant de lire l'oscillation de la lame en fonction du temps. Ajout d'un Curve_fit avec scipy pour obtenir le coefficient d'amortissement.
+"""Script permetant de lire l'oscillation de la lame en fonction du temps. Ajout d'un Curve_fit avec scipy pour obtenir le coefficient d'amortissement ainsi qu'un filtre maison.
 
 Returns:
-    int: Coefficient d'amortissement 
+    int: Coefficient d'amortissement
     matplotlib : Graphique  de l'oscillation de la lame en fonction du temps
 """
 
@@ -20,7 +20,9 @@ if os.path.exists("Résultat prise mesure lame.txt"):
 else:
     print("The file does not exist")
 
-nb_de_set = [1, 2]
+
+
+nb_de_set = [1, 2]  # Set de données à traiter
 Document = open("Résultat prise mesure lame.txt", "x")
 
 ammor = []
@@ -32,28 +34,18 @@ for _ in nb_de_set:
     position = r"Test capteur de position\Prise de mesure {}\F000{}CH1.CSV".format(_, _)
     df = pd.read_csv(position, index_col=False)
 
-    # print(df.head())  # Pour voir la forme du CSV:
-
 
     ################## Sommets des données
 
-    # Peak = find_peaks(df["Tension"], distance=50)
-
-    # Temps_peak = []
-    # Tension_peak = []
-
-    # for i in Peak[0]:
-    #     Temps_peak.append(df["Temps"][i])
-    #     Tension_peak.append(df["Tension"][i])
-
+    # Première étape pour filtrer les données
     Peak = find_peaks(df["Tension"], distance=50)
 
     Temps_peak = []
     Tension_peak = []
 
     for i in Peak[0]:
-        Temps_peak.append(df["Temps"][i])
-        Tension_peak.append(df["Tension"][i])
+        Temps_peak.append(df.iloc[i]["Temps"])
+        Tension_peak.append(df.iloc[i]["Tension"])
 
     index = Tension_peak.index(max(Tension_peak))
 
@@ -72,6 +64,8 @@ for _ in nb_de_set:
     new_dataframe["Don"] = list(Données_filtrées)
     peak_fil = find_peaks(new_dataframe["Don"], distance=50)
 
+    # J'ai maintenant un nouveau data frame que je vais traiter
+
     new_ten= []
     new_tem = []
 
@@ -83,24 +77,13 @@ for _ in nb_de_set:
 
     ################## Curve_fit
 
-    # index = Tension_peak.index(max(Tension_peak))
-
-    # Temps_peak = Temps_peak[index:]
-    # Tension_peak = Tension_peak[index:]
-
     def func(Temps_peak, a, b, d):
         return a * np.exp(-b/Temps_peak) + d
 
     param, param_cova = curve_fit(func, new_tem, new_ten, maxfev=5000)
 
-    # param, param_cova = curve_fit(func, Temps_peak, Tension_peak, maxfev=5000)
-    # # print("Les paramètres sont calculés, l'ammortissement est de: {}".format(param[1])) # Uncomment for result
     ammor.append(param[1])
 
-    # plt.plot(Temps_peak, func(Temps_peak, *param), label="Curve_fit")
-    # plt.plot(Temps_peak, Tension_peak, label="Sommet")
-
-    
 
 
     ##################### Section calcul de la fréquence d'oscillation
@@ -121,17 +104,11 @@ for _ in nb_de_set:
         for i in range(num_peak):
             if i == num_peak - 1:
                 return freqosci.append(np.mean(w_0))
-                # return "La fréquence d'oscillation est de: {} Hz".format(np.mean(w_0)) #Uncomment for result
-                
+
             else:
                 w_0.append(1/(temps[i+1] - temps[i]))
 
     Nombre_sommets = 8  # Sélectionne le nombre de sommet pour le calcul
-
-    # freq_temps = Temps_peak[:Nombre_sommets]
-    # freq_tensi = Tension_peak[:Nombre_sommets]
-    # print(frequence_osci(Temps_peak, Nombre_sommets))
-    # plt.scatter(freq_temps, freq_tensi, color="red", label=r"Sommets utilisés pour $\omega_0$")
 
     freq_temps = new_tem[:Nombre_sommets]
     freq_tensi = new_ten[:Nombre_sommets]
